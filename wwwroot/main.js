244be4543b4dfb920c1b23b12c7e16e80b254965
mikescandy <michele.scandura@outlook.com>
2024-11-06 15:36:27 +0000

    Deploying to gh-pages from @ mikescandy/PastaQuiz@ef0989b61485bcbf8e0a1d6fee57b6ba33e9d250 🚀

---
+++ wwwroot/main.js
@@ -0,0 +1,19 @@
+﻿import { dotnet } from './_framework/dotnet.js'
+
+globalThis.playSound = function (url) {
+    var audio = new Audio(url);
+    audio.play();
+}
+
+const is_browser = typeof window != "undefined";
+if (!is_browser) throw new Error(`Expected to be running in a browser`);
+
+const dotnetRuntime = await dotnet
+    .withDiagnosticTracing(false)
+    .withApplicationArgumentsFromQuery()
+    .create();
+
+const config = dotnetRuntime.getConfig();
+
+await dotnetRuntime.runMain(config.mainAssemblyName, [window.location.search]);
+
